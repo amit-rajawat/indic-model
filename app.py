143ad8d66e25b66daacade4e0074061449cdfee4
Amit Rajawat <129025886+amit-rajawat@users.noreply.github.com>
2023-06-16 21:42:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,29 +30,29 @@ def get_indic_lang_list():
     )
 
 
-@app.route('/translate', methods=['POST'])
-def get_translation():
-    try:
-        text = request.json.get('text')
-        source_lang = request.json.get('source_lang')
-        target_lang = request.json.get('target_lang')
-        start_time = dt.now()
+# @app.route('/translate', methods=['POST'])
+# def get_translation():
+#     try:
+#         text = request.json.get('text')
+#         source_lang = request.json.get('source_lang')
+#         target_lang = request.json.get('target_lang')
+#         start_time = dt.now()
 
-        if text is None or len(text) == 0:
-            return jsonify({"msg": "text should not be empty."}), 400
-        if source_lang != 'en':
-            return jsonify({"msg": "source langauge should be \'en\'."}), 400
-        if target_lang not in INDIC_LANG_CODES:
-            return jsonify({"msg": f"target langauge should be one of {INDIC_LANG_CODES}."}), 400
+#         if text is None or len(text) == 0:
+#             return jsonify({"msg": "text should not be empty."}), 400
+#         if source_lang != 'en':
+#             return jsonify({"msg": "source langauge should be \'en\'."}), 400
+#         if target_lang not in INDIC_LANG_CODES:
+#             return jsonify({"msg": f"target langauge should be one of {INDIC_LANG_CODES}."}), 400
 
-        translation = model.translate_paragraph(text, source_lang, target_lang)
+#         translation = model.translate_paragraph(text, source_lang, target_lang)
 
-        return jsonify({
-            "translation": translation,
-            "model_mttr": int((dt.now() - start_time).total_seconds() * 1000),
-        })
-    except:
-        return jsonify({"msg": "Something went wrong.", }), 500
+#         return jsonify({
+#             "translation": translation,
+#             "model_mttr": int((dt.now() - start_time).total_seconds() * 1000),
+#         })
+#     except:
+#         return jsonify({"msg": "Something went wrong.", }), 500
 
 
 if __name__ == '__main__':
